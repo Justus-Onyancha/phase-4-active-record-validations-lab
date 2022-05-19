40210d2d664e5aea877083004a7904e51eb73a94
Justus-Onyancha <justus.onyancha@moringaschool.com>
2022-05-19 22:16:27 +0300

    completed assignment :Except(custom validations)

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ApplicationRecord
+
+    validates :title, presence: true
+
+    validates :name, presence: true
+
+    validates :name, comparison: {other_than: :name }
+
+    validates :phone, comparison: { equal_to: : 10}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+
+    validates :content, length: { in: 0..250}
+
+    validates :content, length: { maximum: 500}
+
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{Fi} is not a Fiction" }
+
+
+
 end
